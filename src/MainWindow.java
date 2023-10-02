a0c823f9e3d5f94fd27fce31c3221e6290a4b231
Ismael Avila <ismaelg.avilag@gmail.com>
2023-10-02 16:22:28 -0600

    UI finished

---
+++ src/MainWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MainWindow {
+    private JButton buttonLoadFiles;
+    private JButton buttonRunAlgorithm;
+    private JPanel mainPanel;
+    private JPanel panelLoadFiles;
+    private JPanel panelFilesEntered;
+    private JPanel panelMemoryPartitioning;
+    private JPanel panelMemoryManager;
+    private JRadioButton radioButtonFirstFit;
+    private JRadioButton radioButtonBestFit;
+    private JRadioButton radioButtonWorstFit;
+    private JRadioButton radioButtonNextFit;
+    private JTable tableFiles;
+    private JTable tableMemoryPartitioning;
+    private JTable tableOutput;
+}
