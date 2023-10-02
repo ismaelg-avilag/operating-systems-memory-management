54a6f353b8f2c8fad12fc4813c03ccb2a690befd
Ismael Avila <ismaelg.avilag@gmail.com>
2023-10-02 16:43:26 -0600

    MainWindow constructor and main method
    - In the MainWindow constructor a ButtonGroup was created for the radio buttons
    - Button events created
    - Added main method that instantiates the window

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow {
     private JButton buttonLoadFiles;
@@ -15,4 +16,32 @@ public class MainWindow {
     private JTable tableFiles;
     private JTable tableMemoryPartitioning;
     private JTable tableOutput;
+
+
+    public MainWindow()
+    {
+        ButtonGroup group = new ButtonGroup();
+        group.add(radioButtonFirstFit);
+        group.add(radioButtonBestFit);
+        group.add(radioButtonWorstFit);
+        group.add(radioButtonNextFit);
+
+        buttonLoadFiles.addActionListener(e -> {
+
+        });
+
+        buttonRunAlgorithm.addActionListener(e -> {
+
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Algoritmos de Administración de Memoria");
+        frame.setContentPane(new MainWindow().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
